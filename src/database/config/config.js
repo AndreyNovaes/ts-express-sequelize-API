26b32ecd28cab0cf4b8786b3c4b16eb38153a66f
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-07 14:09:20 -0300

    fix: fix dotenv path

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,30 +1,26 @@
-require('dotenv').config();
-
-const {
-  MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOSTNAME, MYSQL_DATABASE, MYSQL_PORT,
-} = process.env;
-console.log(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOSTNAME, MYSQL_DATABASE, MYSQL_PORT);
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
+    username: process.env.MYSQL_USER || 'seuUsername',
+    password: process.env.MYSQL_PASSWORD || 'suaSenha',
     database: process.env.MYSQL_DATABASE || 'ts-express-sequelize-API',
-    host: process.env.HOSTNAME || 'localhost',
-    dialect: 'mysql', // 'mysql' | 'sqlite' | 'postgres' | 'mariadb',
+    host: process.env.MYSQL_HOSTNAME || 'localhost',
+    dialect: 'mysql',
   },
   test: {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'ts-express-sequelize-API',
-    host: process.env.HOSTNAME || 'localhost',
+    host: process.env.MYSQL_HOSTNAME || 'localhost',
     dialect: 'mysql', // 'mysql' | 'sqlite' | 'postgres' | 'mariadb',
   },
   production: {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'ts-express-sequelize-API',
-    host: process.env.HOSTNAME || 'localhost',
+    host: process.env.MYSQL_HOSTNAME || 'localhost',
     dialect: 'mysql', // 'mysql' | 'sqlite' | 'postgres' | 'mariadb',
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
