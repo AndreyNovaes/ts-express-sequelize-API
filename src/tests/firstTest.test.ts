370e45664d5e7cec5aec871bcf664b57956c963f
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-08 11:02:03 -0300

    chore: add axios

--- src/tests/firstTest.test.ts
+++ src/tests/firstTest.test.ts
@@ -1,5 +1,6 @@
+// import request from ;
 
-
-test('/login route without body errors', () => {
-  
-});
+// test('/login route without body errors', () => {
+//   const response = request(app).post('/login');
+//   expect(response.statusCode).toBe(400);
+// });
