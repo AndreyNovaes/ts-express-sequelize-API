4e35f5f3b5d9eba17591bc524168ed271cfb2658
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-08 11:29:45 -0300

    fix: build generating  same testes with js && ts, solved, only using .ts inside src for testing

--- src/tests/firstTest.test.ts
+++ src/tests/firstTest.test.ts
@@ -4,3 +4,8 @@
 //   const response = request(app).post('/login');
 //   expect(response.statusCode).toBe(400);
 // });
+
+test('ee', () => {
+  console.log('esse veio do ts');
+  expect(1).toBe(1);
+});
